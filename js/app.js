608c8c94282e88c5f0ab6629f029790b6b7bfbd9
iamnikhilupadhyay <iamnikhilupadhyay1014@gmail.com>
2018-05-06 09:55:00 +0530

    start

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp',['ngRoute','firebase']);
+
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/login',{
+     templateUrl: 'view/login.html',
+     controller:'RegistrationController'
+    }).
+    when('/register',{
+     templateUrl: '/view/register.html',
+     controller:'RegistrationController'
+    }).
+    when('/success',{
+     templateUrl: '/view/success.html',
+     controller:'SuccessController'
+    }).
+    otherwise({
+      redirectTo:'/login'
+    });
+}])
