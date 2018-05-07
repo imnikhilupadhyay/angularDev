af862d765c733221d97e83c6f2975a8470ea7852
iamnikhilupadhyay <iamnikhilupadhyay1014@gmail.com>
2018-05-07 05:59:39 +0530

    angular node express connection completed excluded mongodb

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,19 +1,11 @@
-myApp.controller('RegistrationController',['$scope','$firebase','$firebaseAuth',function($scope,$firebase,$firebaseAuth){
+myApp.controller('RegistrationController',['$scope',function($scope){
   
-  var ref = $firebase.database().ref();
-  var auth = $firebaseAuth();
-
+ 
   $scope.login = function(){
     $scope.message="Welcome "+$scope.user.email;
   };
 
   $scope.register = function(){
-
-    auth.$createUserWithEmailAndPassword(
-      $scope.user.email,
-      $scope.user.password
-    ).then(function(regUser){
       $scope.message="Hi "+$scope.user.firstname+" "+$scope.user.lastname+", Thanks for registering!!";
-    });
-  };
+    };
 }]);
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const http = require("http");
+const reload = require("reload");
+const path = require("path");
+
+const app = express();
+
+app.set('port',process.env.PORT || 3000); // handling non static port variable
+
+app.use(bodyParser.urlencoded({extended : true})); // to allow nested object extented is set true
+app.use(bodyParser.json());// to catch incoming data
+
+/*mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/DevDB',{
+  useMongoClient : true
+});*/
+//routes(app);
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views',path.join(__dirname+'/view'));
+
+app.use(express.static(path.join(__dirname+'/')));
+//it is a middleware(library) which will provide access to file or folder and return control back to express to hold data
+app.use(require(path.join(__dirname+'/routes/index')));
+/*app.get('/',(req,res)=>
+    //res.send(`Nodes and express server is running on port ${app.get('port')}`)
+    res.sendFile(path.join(__dirname+'/view/index.html'))
+);*/
+
+
+app.listen(app.get('port'), ()=> //listen is callback function
+  console.log(`your server is running on port ${app.get('port')}`)
+);
+
+reload(app);
